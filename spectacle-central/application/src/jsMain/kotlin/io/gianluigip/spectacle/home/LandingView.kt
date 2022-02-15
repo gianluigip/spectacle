f943dcc93dd8ce0de7a5d8bdcfd07ce1313ac0cf
Gianluigi Pierini <gluigip@hotmail.com>
2022-02-15 17:10:39 +0100

    Finished landing page

--- spectacle-central/application/src/jsMain/kotlin/io/gianluigip/spectacle/home/LandingView.kt
+++ spectacle-central/application/src/jsMain/kotlin/io/gianluigip/spectacle/home/LandingView.kt
@@ -1,12 +1,94 @@
 package io.gianluigip.spectacle.home
 
+import csstype.Color
+import csstype.Display
+import csstype.JustifyContent
+import csstype.TextAlign.Companion.justify
+import csstype.pct
+import csstype.px
+import io.gianluigip.spectacle.navigation.GITHUB_URL
+import io.gianluigip.spectacle.specification.component.specificationsReportPath
+import kotlinext.js.jso
+import mui.material.Box
+import mui.material.Link
+import mui.material.Paper
+import mui.material.Stack
 import mui.material.Typography
+import mui.system.ResponsiveStyleValue
 import react.FC
 import react.Props
+import react.css.css
+import react.router.dom.NavLink
+import react.useContext
 
 val LandingView = FC<Props> {
+    val theme by useContext(ThemeContext)
 
+    Box {
+        sx = jso {
+            display = Display.flex
+            justifyContent = JustifyContent.center
+        }
+
+        Paper {
+            sx = jso { width = 100.pct; maxWidth = 1000.px; padding = 20.px }
+            elevation = 3
+
+            Stack {
+                spacing = ResponsiveStyleValue(1)
+                Typography {
+                    css { color = Color(theme.palette.info.main) }
+                    variant = "h5"; +"Repository of Living Documentation for Distributed Systems"
+                }
+                Paragraph {
+                    text = """
+                In modern distributed systems the behavior of the product is defined across multiple components and services, and usually developed 
+                by multiple teams, as a result, the documentation is fragmented and difficult to find.
+                """
+                }
+                Paragraph {
+                    text = """       
+                Additionally, the system tends to change faster than the documentation which makes the latter unreliable.
+                """
+                }
+                Paragraph {
+                    text = """       
+                Spectacle provides a solution for generating documentation from the multiple codebases in a modern system and then storing it in a 
+                central repository that anyone can access.
+                """
+                }
+
+                NavLink {
+                    to = specificationsReportPath
+                    css { color = Color(theme.palette.info.main) }
+                    Typography {
+                        variant = "h5"; +"Specifications"
+                    }
+                }
+                Paragraph {
+                    text = """       
+                You can search and bookmark any specification published, this allows you to easily share the behavior of the product with your 
+                stakeholder, and to consolidate the documentation of features that could be defined across several teams and components.
+                """
+                }
+
+                Typography {
+                    css { color = Color(theme.palette.info.main) }
+                    variant = "h6"; +"Further Instruction and Documentation in our GitHub page"
+                }
+                Link { href = GITHUB_URL; +GITHUB_URL }
+            }
+        }
+    }
+}
+
+external interface ParagraphProps : Props {
+    var text: String
+}
+
+val Paragraph = FC<ParagraphProps> {
     Typography {
-        +"Welcome to Spectacle Central"
+        css { textAlign = justify }
+        +it.text
     }
 }
--- spectacle-central/application/src/jsMain/kotlin/io/gianluigip/spectacle/navigation/Header.kt
+++ spectacle-central/application/src/jsMain/kotlin/io/gianluigip/spectacle/navigation/Header.kt
@@ -8,13 +8,19 @@ import io.gianluigip.spectacle.home.GridAreas
 import io.gianluigip.spectacle.home.ThemeContext
 import io.gianluigip.spectacle.home.Themes
 import io.gianluigip.spectacle.common.component.Spacer
+import io.gianluigip.spectacle.common.utils.toNode
 import io.gianluigip.spectacle.specification.component.specificationsReportPath
 import kotlinext.js.jso
+import kotlinx.browser.window
 import mui.icons.material.Brightness4
 import mui.icons.material.Brightness7
+import mui.icons.material.GitHub
 import mui.material.AppBar
 import mui.material.AppBarPosition
 import mui.material.Box
+import mui.material.IconButton
+import mui.material.IconButtonColor
+import mui.material.Size
 import mui.material.Switch
 import mui.material.Toolbar
 import mui.material.Tooltip
@@ -22,8 +28,12 @@ import react.FC
 import react.Props
 import react.ReactNode
 import react.create
+import react.dom.aria.AriaHasPopup
+import react.dom.aria.ariaHasPopup
 import react.dom.aria.ariaLabel
 import react.useContext
+
+const val GITHUB_URL = "https://github.com/gianluigip/spectacle"
 
 val Header = FC<Props> {
     var theme by useContext(ThemeContext)
@@ -64,6 +74,18 @@ val Header = FC<Props> {
                     onChange = { _, checked ->
                         theme = if (checked) Themes.Dark else Themes.Light
                     }
+                }
+            }
+
+            Tooltip {
+                title = "Go to Source".toNode()
+                IconButton {
+                    ariaLabel = "Source Code"
+                    ariaHasPopup = AriaHasPopup.`false`
+                    size = Size.large
+                    color = IconButtonColor.inherit
+                    onClick = { window.location.href = GITHUB_URL }
+                    GitHub()
                 }
             }
         }
