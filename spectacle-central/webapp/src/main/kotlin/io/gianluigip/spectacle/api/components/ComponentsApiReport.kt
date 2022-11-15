e511b8c48b52d661f0118cae6548d5d43cacb7c1
Gianluigi Pierini <gluigip@hotmail.com>
2022-11-15 13:26:09 +0100

    Improve ordering of endpoints

--- spectacle-central/webapp/src/main/kotlin/io/gianluigip/spectacle/api/components/ComponentsApiReport.kt
+++ spectacle-central/webapp/src/main/kotlin/io/gianluigip/spectacle/api/components/ComponentsApiReport.kt
@@ -37,11 +37,21 @@ val ComponentsApiReport = FC<ComponentsApiReportProps> { props ->
                 SectionTitle { text = component.component; color = theme.palette.info.contrastText }
             }
             AccordionDetails {
-                component.endpoints.sortedBy { "${it.path}-${it.method}" }.forEach {
+                component.endpoints.sortedBy { "${it.path}-${it.method.toMethodNumber()}" }.forEach {
                     ApiEndpointCard { endpoint = it }
                 }
             }
         }
     }
 
+}
+
+private fun String.toMethodNumber() = when (uppercase()) {
+    "GET" -> 1
+    "PATCH" -> 2
+    "OPTIONS" -> 3
+    "PUT" -> 4
+    "POST" -> 5
+    "DELETE" -> 6
+    else -> 99
 }
